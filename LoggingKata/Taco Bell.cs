b952c110cb09107811ba7464c5d059282cfe1916
Paul Gearo <PaulGearo@gmail.com>
2019-02-15 20:45:34 -0600

    final product

--- LoggingKata/Taco Bell.cs
+++ LoggingKata/Taco Bell.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LoggingKata
 {
-    class TacoBell : ITrackable
+    public class TacoBell : ITrackable
     {
         public string Name { get; set; }
         public Point Location { get; set; }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -6,16 +6,25 @@
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
-
+        /// <summary>
+        /// Parses the file ya dummy
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
         public ITrackable Parse(string line)
         {
             logger.LogInfo("Begin parsing");
 
             // Do not fail if one record parsing fails, return null
             //return null; // TODO Implement
+            if (line == null)
+            {
+                return null;
+            }
 
             // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
             var cells = line.Split(',');
+
 
             // If your array.Length is less than 3, something went wrong
             if (cells.Length < 3)
@@ -32,11 +41,21 @@
             var locationName = cells[2];
 
             // Your going to need to parse your string as a `double`
-            double latitude = double.Parse(latitudeString);
-            double longitude = double.Parse(longitudeString);
+            if (!double.TryParse(latitudeString, out double latitude))
+                return null;
+            if (!double.TryParse(longitudeString, out double longitude))
+                return null;
 
 
             // which is similar to parsing a string as an `int`
+            if (latitude < -90 || latitude > 90)
+                return null;
+            if(longitude < -180 || longitude > 180)
+                return null;
+
+
+
+
 
             // You'll need to create a TacoBell class
             // that conforms to ITrackable
